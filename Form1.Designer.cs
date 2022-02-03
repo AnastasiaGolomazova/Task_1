e39f9e5786ee0921c46d7d055f7cfc4d4931251d
AnastasiaGolomazova <66026569+AnastasiaGolomazova@users.noreply.github.com>
2022-02-04 02:58:51 +0700

    Loading the map

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,46 @@ namespace Task_1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gMap = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gMap
+            // 
+            this.gMap.Bearing = 0F;
+            this.gMap.CanDragMap = true;
+            this.gMap.GrayScaleMode = false;
+            this.gMap.LevelsKeepInMemmory = 5;
+            this.gMap.Location = new System.Drawing.Point(12, 12);
+            this.gMap.MarkersEnabled = true;
+            this.gMap.MaxZoom = 2;
+            this.gMap.MinZoom = 2;
+            this.gMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMap.Name = "gMap";
+            this.gMap.NegativeMode = false;
+            this.gMap.PolygonsEnabled = true;
+            this.gMap.RetryLoadTile = 0;
+            this.gMap.RoutesEnabled = true;
+            this.gMap.ShowTileGridLines = false;
+            this.gMap.Size = new System.Drawing.Size(757, 584);
+            this.gMap.TabIndex = 0;
+            this.gMap.Zoom = 0D;
+            this.gMap.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1047, 608);
+            this.Controls.Add(this.gMap);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMap;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.ToolTips;
+using GMap.NET.WindowsForms.Markers;
+
+
+
 namespace Task_1
 {
     public partial class Form1 : Form
@@ -15,6 +23,31 @@ namespace Task_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache; 
+            gMap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance; 
+            gMap.MinZoom = 2;
+            gMap.MaxZoom = 16; 
+            gMap.Zoom = 13; 
+            gMap.Position = new GMap.NET.PointLatLng(56.50373683619619, 84.94823803271237);
+            gMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            gMap.CanDragMap = true; 
+            gMap.DragButton = MouseButtons.Left; 
+            gMap.Show(); 
+            gMap.ShowTileGridLines = false; 
+            gMap.Bearing = 0;
+            gMap.CanDragMap = true;
+            gMap.DragButton = MouseButtons.Left;
+            gMap.GrayScaleMode = false;
+            gMap.MarkersEnabled = true;
+            gMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+
+
+
+
         }
     }
 }
